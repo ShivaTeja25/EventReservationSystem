1430c4b51ca90699ab987fffa89fa3980a851580
ShivaTeja25 <shivateja252709@gmail.com>
2025-03-13 14:57:22 -0400

    modified Data class

--- EventReservation/src/main/java/com/media/EventReservation/Model/EventData.java
+++ EventReservation/src/main/java/com/media/EventReservation/Model/EventData.java
@@ -1,6 +1,8 @@
 package com.media.EventReservation.Model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,18 +15,50 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class EventData {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int eventid;
     private String eventName;
     private String hostName;
     private String hostEmail;
     private String eventLocation;
+    private String deptName;
+    private String micsNeeded;
+    private String recording;
+    private String eventType;
+    private String computerAsst;
+    private String conferenceTool;
+    private String wifiAccess;
+    private String extraInformation;
 
-    public int getEventid() {
-        return eventid;
+    public EventData() {
+        this.eventName = "Default Event";
+        this.hostName = "Default Host";
+        this.hostEmail = "default@example.com";
+        this.eventLocation = "Default Location";
+        this.deptName = "Default Department";
+        this.micsNeeded = "No";
+        this.recording = "No";
+        this.eventType = "In-Person";
+        this.computerAsst = "No";
+        this.conferenceTool = "None";
+        this.wifiAccess = "No";
+        this.extraInformation = "No additional info";
     }
 
-    public void setEventid(int eventid) {
-        this.eventid = eventid;
+
+    public EventData(String eventName, String hostName, String hostEmail, String eventLocation, String recording, String eventType, String micsNeeded, String deptName, String computerAsst, String conferenceTool, String wifiAccess, String extraInformation) {
+        this.eventName = eventName;
+        this.hostName = hostName;
+        this.hostEmail = hostEmail;
+        this.eventLocation = eventLocation;
+        this.recording = recording;
+        this.eventType = eventType;
+        this.micsNeeded = micsNeeded;
+        this.deptName = deptName;
+        this.computerAsst = computerAsst;
+        this.conferenceTool = conferenceTool;
+        this.wifiAccess = wifiAccess;
+        this.extraInformation = extraInformation;
     }
 
     public String getEventName() {
@@ -57,5 +91,69 @@ public class EventData {
 
     public void setEventLocation(String eventLocation) {
         this.eventLocation = eventLocation;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public String getMicsNeeded() {
+        return micsNeeded;
+    }
+
+    public void setMicsNeeded(String micsNeeded) {
+        this.micsNeeded = micsNeeded;
+    }
+
+    public String getRecording() {
+        return recording;
+    }
+
+    public void setRecording(String recording) {
+        this.recording = recording;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getComputerAsst() {
+        return computerAsst;
+    }
+
+    public void setComputerAsst(String computerAsst) {
+        this.computerAsst = computerAsst;
+    }
+
+    public String getConferenceTool() {
+        return conferenceTool;
+    }
+
+    public void setConferenceTool(String conferenceTool) {
+        this.conferenceTool = conferenceTool;
+    }
+
+    public String getWifiAccess() {
+        return wifiAccess;
+    }
+
+    public void setWifiAccess(String wifiAccess) {
+        this.wifiAccess = wifiAccess;
+    }
+
+    public String getExtraInformation() {
+        return extraInformation;
+    }
+
+    public void setExtraInformation(String extraInformation) {
+        this.extraInformation = extraInformation;
     }
 }
